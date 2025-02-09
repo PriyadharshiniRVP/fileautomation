0aae022b58f08d22e260d2b687291a65b1f298a5
PriyadharshiniRVP <146438685+PriyadharshiniRVP@users.noreply.github.com>
2025-02-09 19:36:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,1 @@
-from flask import Flask, render_template
-import psutil
 
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    cpu_usage = psutil.cpu_percent()
-    ram_usage = psutil.virtual_memory().percent
-    disk_usage = psutil.disk_usage('/').percent
-    return render_template('index.html', cpu=cpu_usage, ram=ram_usage, disk=disk_usage)
-
-if __name__ == '__main__':
-    app.run(debug=True)
